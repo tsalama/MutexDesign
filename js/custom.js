ce21f9bdee117ebf143cd1af636b52a9123e8745
Tarek Salama <tsalama@asu.edu>
2018-06-22 17:36:02 -0700

    Testimonials & Carousel

--- js/custom.js
+++ js/custom.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
         itemsMobile: [479, 1],
         singleItem: false,
         startDragging: true,
-        autoPlay: true
+        autoPlay: 12000
     });
 
 });
